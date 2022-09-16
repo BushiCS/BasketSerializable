568e17775381dca37bdb8ee7613dbe3704ad58b3
Yuriy Sviridov <crip.shooter@gmail.com>
2022-09-16 18:17:38 +0300

    Add Main & Basket

---
+++ src/Basket.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.Arrays;
+
+import static java.lang.System.out;
+
+public class Basket {
+    private int sumProducts;
+    private final int[] prices;
+    private final String[] products;
+    private int[] purchasesCount;
+
+    public Basket(int[] prices, String[] products) {
+        this.prices = prices;
+        this.products = products;
+        purchasesCount = new int[prices.length];
+    }
+
+    public void printCatalog() { // вывод товаров
+        out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < products.length; i++) {
+            out.println((i
+                    + 1)
+                    + ". "
+                    + products[i]
+                    + " "
+                    + prices[i]
+                    + " руб/шт");
+        }
+    }
+
+    public void addToCart(int productNumber, int productCount) { // добавление товара в корзину
+        out.println(products[productNumber] + " " + (productCount + purchasesCount[productNumber]) + " шт. (по цене " + prices[productNumber] + " руб. за штуку)");
+        purchasesCount[productNumber] += productCount; // общее кол-во взятого товара[номер выбранного товара] += кол-во выбранного товара
+        out.println("Сумма: " + (prices[productNumber] * purchasesCount[productNumber]) + " руб.");
+    }
+
+    public void printCart() { // подсчет и вывод всей корзины
+        out.println("Ваша корзина: ");
+        for (int i = 0; i < purchasesCount.length; i++) {
+            if (purchasesCount[i] != 0) {
+                int sum = prices[i] * purchasesCount[i];
+                sumProducts += sum;
+                out.println(products[i]
+                        + " - "
+                        + purchasesCount[i]
+                        + " шт. "
+                        + "("
+                        + prices[i]
+                        + " руб/шт): "
+                        + sum
+                        + " руб в сумме");
+            }
+        }
+        out.println("Итого: " + sumProducts + " руб");
+    }
+
+    public void saveTxt(File textFile) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(textFile))) {
+            for (int i = 0; i < products.length; i++) {
+                if (purchasesCount[i] != 0) {
+                    writer.write(products[i] + " " + purchasesCount[i] + " " + prices[i] + " ");
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        try (BufferedReader reader = new BufferedReader(new BufferedReader(new FileReader(textFile)))) {
+            if (reader.ready()) {
+                String[] readerArray = reader.lines().toArray(String[]::new); // разбив на массив строк
+                String[] stringSplitArray; // массив для разделения строк
+                String[] productsLoad = new String[readerArray.length];
+                String[] countLoad = new String[readerArray.length];
+                String[] pricesLoad = new String[readerArray.length];
+                for (int i = 0; i < readerArray.length; i++) {
+                    stringSplitArray = readerArray[i].split(" "); // разделение каждой строки (ячейки массива) (на каждой итерации) в массив
+                    productsLoad[i] = stringSplitArray[0];  // присвоение в нужный массив нужных данных
+                    countLoad[i] = stringSplitArray[1];
+                    pricesLoad[i] = stringSplitArray[2];
+                }
+                int[] intCountLoad = Arrays.stream(countLoad).mapToInt(Integer::parseInt).toArray();
+                int[] intPricesLoad = Arrays.stream(pricesLoad).mapToInt(Integer::parseInt).toArray();
+                Basket basketLoad = new Basket(intPricesLoad, productsLoad);
+                basketLoad.purchasesCount = intCountLoad;
+                return basketLoad;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int[] getPurchasesCount() {
+        return purchasesCount;
+    }
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 import static java.lang.System.out;
@@ -5,137 +6,39 @@ import static java.lang.System.out;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] products = new String[]{"Хлеб", "Яблоки", "Молоко", "Сахар", "Соль"};
-        int[] prices = new int[]{50, 20, 80, 60, 30};
-        int[] purchasesCount = new int[products.length];
+        String[] products = new String[]{"Хлеб", "Яблоки", "Молоко"};
+        int[] prices = new int[]{50, 20, 80};
+        File backupFile = new File("basket.txt");
+        Basket basket;
 
-        String[] saleProducts = new String[]{"Яйца", "Масло", "Гречка"};
-        int[] salePrices = new int[]{80, 100, 90};
-        int[] salePurchasesCount = new int[saleProducts.length];
-
-        out.println("Список возможных товаров для покупки");
-        for (int i = 0; i < products.length; i++) {
-            out.println((i
-                    + 1)
-                    + ". "
-                    + products[i]
-                    + " "
-                    + prices[i]
-                    + " руб/шт");
+        if (backupFile.exists()) {
+            if (Basket.loadFromTxtFile(backupFile) != null) {
+                basket = Basket.loadFromTxtFile(backupFile);
+            } else {
+                basket = new Basket(prices, products);
+            }
+        } else {
+            basket = new Basket(prices, products);
         }
-        out.println("Список товаров по акции");
-        for (int i = 0; i < saleProducts.length; i++) {
-            out.println(i
-                    + (products.length + 1)
-                    + ". "
-                    + saleProducts[i]
-                    + " "
-                    + salePrices[i]
-                    + " руб/шт");
-        }
-        int sumProducts = 0;
-        int productNumber;
-        int productCount;
-        int sum;
-        int saleSum;
+        assert basket != null;
+        basket.printCatalog();
         while (true) {
-            out.println("Выберите товар и количество или введите `end` "); // ВЫХОД
+            System.out.println("Выберите товар и количество или введите `end` ");
             String input = sc.nextLine();
             if (input.equals("end")) {
                 break;
             }
-            String[] parts = input.split(" "); // РАЗДЕЛЕНИЕ
-            if (parts.length != 2) {
-                out.println("Некорректный ввод 2 значений, нужно ввести 2 числа");
+            String[] parts = input.split(" ");
+            int productNumber = Integer.parseInt(parts[0]) - 1;
+            int productCount = Integer.parseInt(parts[1]);
+            if (productCount == 0) {
+                basket.getPurchasesCount()[productNumber] = 0;
+                out.println("Продукт обнулен");
                 continue;
             }
-            try {
-                productNumber = Integer.parseInt(parts[0]) - 1; // ВВОД
-                productCount = Integer.parseInt(parts[1]);
-            } catch (NumberFormatException e) {
-                out.println("Неверный формат ввода");
-                continue;
-            }
-
-            if ((productNumber > (products.length + saleProducts.length) - 1 || productNumber < 0)) {
-                out.println("Неверный продукт");
-                continue;
-            }
-            if (productNumber < products.length) { // 1 массив
-                if (productCount == 0) {
-                    purchasesCount[productNumber] = 0;
-                    out.println("Вы полностью убрали из корзины: " + products[productNumber]);
-                    continue;
-                } else if (productCount < 0) {
-                    purchasesCount[productNumber] += productCount;
-                    if (purchasesCount[productNumber] < 0) {
-                        purchasesCount[productNumber] = 0;
-                        out.println("Вы полностью убрали из корзины: " + products[productNumber]);
-                    } else {
-                        out.println("Количество продукта " + products[productNumber] + " уменьшено на " + productCount + " шт.");
-                    }
-                    continue;
-                }
-                out.println(products[productNumber]
-                        + " "
-                        + (productCount + purchasesCount[productNumber])
-                        + " шт.");
-                purchasesCount[productNumber] += productCount;
-            } else { // 2 массив (скидки)
-                int saleProductNumber = productNumber - products.length;
-                if (productCount == 0) {
-                    salePurchasesCount[saleProductNumber] = 0;
-                    out.println("Вы полностью убрали из корзины: " + saleProducts[saleProductNumber]);
-                    continue;
-                } else if (productCount < 0) {
-                    salePurchasesCount[saleProductNumber] += productCount;
-                    if (salePurchasesCount[saleProductNumber] < 0) {
-                        salePurchasesCount[saleProductNumber] = 0;
-                        out.println("Вы полностью убрали из корзины: " + saleProducts[saleProductNumber]);
-                    } else {
-                        out.println("Количество продукта " + saleProducts[saleProductNumber] + " уменьшено на " + productCount + " шт.");
-                    }
-                    continue;
-                }
-                out.println(saleProducts[saleProductNumber]
-                        + " "
-                        + (productCount + salePurchasesCount[saleProductNumber])
-                        + " шт. ");
-                salePurchasesCount[saleProductNumber] += productCount;
-            }
+            basket.addToCart(productNumber, productCount); // добавление товара и его кол-во в корзину
         }
-        out.println("Ваша корзина: ");
-        for (int i = 0; i < products.length; i++) {
-            if (purchasesCount[i] != 0) {
-                sum = prices[i] * purchasesCount[i];
-                sumProducts += sum;
-                out.println(products[i]
-                        + " - "
-                        + purchasesCount[i]
-                        + " шт. "
-                        + "("
-                        + prices[i]
-                        + " руб/шт): "
-                        + sum
-                        + " руб в сумме");
-            }
-        }
-        for (int i = 0; i < saleProducts.length; i++) {
-            if (salePurchasesCount[i] != 0) {
-                saleSum = (2 * (salePurchasesCount[i] / 3) + salePurchasesCount[i] % 3) * salePrices[i];
-                sumProducts += saleSum;
-                out.println(saleProducts[i]
-                        + " - "
-                        + salePurchasesCount[i]
-                        + " шт. "
-                        + "("
-                        + salePrices[i]
-                        + "руб/шт): "
-                        + saleSum
-                        + " руб в сумме");
-            }
-        }
-        out.println("Итого: " + sumProducts + " руб");
+        basket.printCart(); // подсчет и вывод корзины
+        basket.saveTxt(new File("basket.txt"));
     }
 }
-
